b46992141b2cacee9b811581ca442e51ab52d688
Asif Estiak <me.asif.ae@gmail.com>
2021-06-03 18:16:06 +0600

    updated weather details

--- components/WeatherDetails/WeatherDetails.js
+++ components/WeatherDetails/WeatherDetails.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../utils/index';
 
-const componentName = () => (
-    <View>
-        <Text>componentName</Text>
+const { PRIMARY_COLOR, SECONDERY_COLOR, BORDER_COLOR } = colors;
+
+const componentName = ({ currentWeather }) => {
+  const {main: {feels_like, humidity}} = currentWeather;
+  return (
+    <View style={styles.weatherDetails}>
+      <View style={styles.weatherDetailsRow}>
+        <View style={{...styles.weatherDetailsBox, borderRight}}>
+          <Text>{feels_like}</Text>
+        </View>
+        <View style={styles.weatherDetailsBox}>
+          <Text>{humidity}</Text>
+        </View>
+      </View>
     </View>
-);
+  );
+}
 
 export default componentName;
 
 const styles = StyleSheet.create({
   weatherDetails: {
     marginTop: 'auto',
+    margin: 15,
     borderWidth: 1,
-  }
+    borderColor: BORDER_COLOR,
+    borderRadius: 10,
+  },
+  weatherDetailsRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  weatherDetailsBox: {
+    flex: 1,
+    padding: 20,
+  },
 })
