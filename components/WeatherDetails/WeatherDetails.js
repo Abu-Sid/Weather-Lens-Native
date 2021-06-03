d51424728e82503bca7d0b12a3a7b1f88308b348
Asif Estiak <me.asif.ae@gmail.com>
2021-06-03 17:43:25 +0600

    added some styles

---
+++ components/WeatherDetails/WeatherDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const componentName = () => (
+    <View>
+        <Text>componentName</Text>
+    </View>
+);
+
+export default componentName;
+
+const styles = StyleSheet.create({
+  weatherDetails: {
+    marginTop: 'auto',
+    borderWidth: 1,
+  }
+})
