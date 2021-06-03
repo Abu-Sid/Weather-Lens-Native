7676bbbd45a8a12e4ba3309622ac5bfe819b7e39
lincoln khan <mdfaysalkhancse@gmail.com>
2021-06-03 18:09:22 +0600

    weatherinfo components already finished

--- components/WeatherInfo/WeatherInfo.js
+++ components/WeatherInfo/WeatherInfo.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 
 export default function WeatherInfo({currentWeather}) {
-    const {main:{temp},
-    weather: [details]
+
+    const { 
+    main:{temp},
+    weather: [details],
+    name,
 } = currentWeather;
-const {icon} = details
+const {icon, main, description} = details
+
 const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
     return (
         <View style = {styles.weatherInfo}>
-            <Image source ={{uri : iconUrl}} />
+            <Text>{name}</Text>
+            <Image style = {styles.weatherIcon} source ={{uri : iconUrl}} />
             <Text>{temp}</Text>
+            <Text style = {styles.weatherDescription}>{description}</Text>
+            <Text>{main}</Text>
         </View>
     )
 }
@@ -18,5 +25,12 @@ const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
 const styles = StyleSheet.create({
     weatherInfo : {
         alignItems: 'center',
+    },
+    weatherIcon: {
+        width: 100,
+        height: 100,
+    },
+    weatherDescription : {
+        textTransform: 'capitalize'
     }
 })
